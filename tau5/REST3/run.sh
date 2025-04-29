dfd691b4f15724ca3d98386788170ffb90ca3637
Kai Zhu <22319143@zju.edu.cn>
2025-04-29 14:13:31 +0800

    Create run.sh

---
+++ tau5/REST3/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+now=$PWD
+newdir=./
+nsteps=2500000000 # 5us
+#mpirun -np 8  gmx_mpi mdrun -plumed $now/plumed.dat -multidir 0 1 2 3 4 5 6 7 -replex 1000  -nsteps 100000 -hrex -dlb no -s topol.tpr -deffnm prod
+mpirun -np 8  gmx_mpi mdrun -plumed $now/plumed.dat -multidir 0 1 2 3 4 5 6 7 -replex 1000  -nsteps $nsteps -hrex -dlb no -s topol.tpr -cpi prod.cpt -deffnm prod
