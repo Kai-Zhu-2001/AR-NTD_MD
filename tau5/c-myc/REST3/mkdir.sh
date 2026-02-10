c539143ed63bb537995bbab5cd91cbee08522612
Kai Zhu <22319143@zju.edu.cn>
2026-02-10 18:55:29 +0100

    Delete c-myc files

--- tau5/c-myc/REST3/mkdir.sh
+++
@@ -1,8 +0,0 @@
-    mkdir -p 0; cp setup/topol0.tpr 0/topol.tpr
-    mkdir -p 1; cp setup/topol1.tpr 1/topol.tpr
-    mkdir -p 2; cp setup/topol2.tpr 2/topol.tpr
-    mkdir -p 3; cp setup/topol3.tpr 3/topol.tpr
-    mkdir -p 4; cp setup/topol4.tpr 4/topol.tpr
-    mkdir -p 5; cp setup/topol5.tpr 5/topol.tpr
-    mkdir -p 6; cp setup/topol6.tpr 6/topol.tpr
-    mkdir -p 7; cp setup/topol7.tpr 7/topol.tpr
